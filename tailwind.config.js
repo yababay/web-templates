978dd3af8065c902dd8892a02ab91dcf844b6997
mabel <bigbelk@live.ru>
2023-01-20 12:29:10 +0300

    postinstall

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        xs: '420px',
+      },
+    },
+  },
+  plugins: [],
+}
